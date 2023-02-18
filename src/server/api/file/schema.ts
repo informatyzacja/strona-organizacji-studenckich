0efe8eae9a70e989d8aa8aa487c5b75241113cdc
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-02-18 23:52:46 +0100

    fix(admin): remove svg image due to xss attacks

--- src/server/api/file/schema.ts
+++ src/server/api/file/schema.ts
@@ -2,12 +2,7 @@ import { z } from "zod";
 
 export const maxFileSize = 3 * 1024 * 1024;
 
-export const acceptedImageTypes = [
-  "image/png",
-  "image/jpeg",
-  "image/svg+xml",
-  "image/webp",
-];
+export const acceptedImageTypes = ["image/png", "image/jpeg", "image/webp"];
 
 export const uploadResponseSchema = z.object({
   url: z.string(),
