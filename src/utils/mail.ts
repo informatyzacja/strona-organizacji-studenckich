f6257f50ea8449582c4069ce00320ef04c63f066
Byczax <byczqq136@gmail.com>
2023-02-19 22:56:00 +0100

    fix: why i did this... nevermind.

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -2,15 +2,20 @@ import nodemailer from "nodemailer";
 import type { SendMailOptions } from "nodemailer";
 import { env } from "@/env.mjs";
 
-const transporter = nodemailer.createTransport({
-  host: env.SMTP_HOST,
-  port: 587,
-  secure: true,
-  auth: {
-    user: env.SMTP_USERNAME,
-    pass: env.SMTP_PASSWORD,
+const transporter = nodemailer.createTransport(
+  {
+    host: env.SMTP_HOST,
+    port: 587,
+    secure: true,
+    auth: {
+      user: env.SMTP_USERNAME,
+      pass: env.SMTP_PASSWORD,
+    },
   },
-});
+  {
+    from: env.EMAIL_FROM,
+  }
+);
 
 const sendMail = async (
   to: string,
@@ -25,21 +30,6 @@ const sendMail = async (
     text,
     html,
   } satisfies SendMailOptions;
-
-  const transporter = nodemailer.createTransport(
-    {
-      host: env.SMTP_HOST,
-      port: 587,
-      secure: true,
-      auth: {
-        user: env.SMTP_USERNAME,
-        pass: env.SMTP_PASSWORD,
-      },
-    },
-    {
-      from: env.EMAIL_FROM,
-    }
-  );
 
   const info = await transporter.sendMail(mailOptions);
   console.log("Message sent: %s", info.messageId);
